26ab6117170e1bc75ba57fe7cff7f902f937e52b
tsm <attila.egyed.91@gmail.com>
2016-12-15 13:36:36 +0100

    * update dependecies * cleanup * fix linting errors * replace deep-freeze with deep-freeze-strict * bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ActionReducer } from '@ngrx/store';
-import * as deepFreeze from 'deep-freeze';
+import * as deepFreeze from 'deep-freeze-strict';
 
 /**
  * Middleware that prevents state from being mutated anywhere in the app.
@@ -9,7 +9,7 @@ export function storeFreeze(reducer): ActionReducer<any> {
     return function (state = {}, action) {
 
         deepFreeze(state);
-        
+
         // guard against trying to freeze null or undefined types
         if (action.payload) {
             deepFreeze(action.payload);
